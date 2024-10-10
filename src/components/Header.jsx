338cc6456a75671fd636de0d3da9fe01c3efedff
Moshiur Tanvin Evan <96300089+mtevan@users.noreply.github.com>
2024-10-11 01:11:20 +0600

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
         <header className="app-header">
-            <a href="https://mteverse.vercel.app/"><img className="logo" src="/ScreenShots/logo.png alt="MTEVERSE"/></a>
+            <a href="https://mteverse.vercel.app/"><img className="logo" src="/ScreenShots/logo.png" alt="MTEVERSE"/></a>
             <h1>MTEverse Weather Reporter</h1>
         </header>
     );
